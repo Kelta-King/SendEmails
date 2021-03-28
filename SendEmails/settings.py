0a89f7dac58cdd93fe8de57268af8ed364bfb01a
Kushang Shah <kushangshah131@gmail.com>
2021-03-28 17:58:24 +0530

    tempates

--- SendEmails/settings.py
+++ SendEmails/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'emailSend'
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'SendEmails.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- emailSend/urls.py
+++ emailSend/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', include('emailSend.urls')),
+    path('', views.homePage),
 ]
